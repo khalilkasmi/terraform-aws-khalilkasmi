f649dc55e2f6d7e20dc7417182a2cf997518973e
khalilkasmi <khalilkasmi1997@gmail.com>
2022-06-28 16:30:46 +0200

    added module

--- ubuntu/variables.tf
+++ ubuntu/variables.tf
@@ -1,12 +1,15 @@
 variable "region" {
   description = "AWS region"
+  type        = string
 }
 
 variable "instance_type" {
   description = "Type of EC2 instance to provision"
+  type        = string
 }
 
 variable "instance_name" {
   description = "EC2 instance name"
+  type        = string
 }
 

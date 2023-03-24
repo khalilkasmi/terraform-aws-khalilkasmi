2c83ea46687c06a21c4017ffb44edfce2bf16cdc
khalil kasmi <khalilkasmi1997@gmail.com>
2023-03-24 22:34:14 +0100

    Add comments to code using GPT-4

--- ubuntu/main.tf
+++ ubuntu/main.tf
@@ -1,3 +1,6 @@
+# "aws_caller_identity" "current" {}
+
+The purpose of the code block is to retrieve information about the AWS account that is making the call. This information can include the account ID, the ARN, and the user
 data "aws_ami" "ubuntu" {
   most_recent = true
 
@@ -14,8 +17,21 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
+# "aws_db_instance" "mydb" {
+  allocated_storage    = 5
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.
+
+
 resource "aws_instance" "ubuntu" {
-  ami           = data.aws_ami.ubuntu.id
+  ami           = # "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.
+ data.aws_ami.ubuntu.id
   instance_type = var.instance_type
 
   tags = {

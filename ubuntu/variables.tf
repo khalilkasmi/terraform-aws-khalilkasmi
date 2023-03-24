db5304d0246713ed8094423424eb65e593d6069a
khalil kasmi <khalilkasmi1997@gmail.com>
2023-03-24 22:34:20 +0100

    Add comments to code using GPT-4

--- ubuntu/variables.tf
+++ ubuntu/variables.tf
@@ -1,12 +1,35 @@
+# "environment" {
+  type        = string
+  default     = "dev"
+  description = "Environment to deploy"
+}
+
+The purpose of this code block is to define a Terraform variable named "environment" with a
 variable "region" {
   description = "AWS region"
   type        = string
 }
 
+# "environment" {
+  type        = "string"
+  default     = "prod"
+  description = "The environment to deploy to"
+}
+
+The code block creates a variable named "environment" with a type of
+
+
 variable "instance_type" {
   description = "Type of EC2 instance to provision"
   type        = string
 }
+
+# "environment" {
+  default = "dev"
+}
+
+This Terraform code block defines a variable called "environment" with a default value of "dev". This variable can be used in other parts of the Terraform configuration to specify
+
 
 variable "instance_name" {
   description = "EC2 instance name"

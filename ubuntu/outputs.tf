e1229716cb8d9827ec2a7f7a1170d0e16f194f7e
khalil kasmi <khalilkasmi1997@gmail.com>
2023-03-24 22:34:17 +0100

    Add comments to code using GPT-4

--- ubuntu/outputs.tf
+++ ubuntu/outputs.tf
@@ -1,6 +1,18 @@
+# "aws_account_id" {
+  value = "${lookup(var.aws_accounts, var.aws_account_name)}"
+}
+
+The purpose of this code block is to output the AWS account ID
 output "instance_ami" {
   value = aws_instance.ubuntu.ami
 }
+
+# "instance_ip" {
+  value = "${aws_instance.web.public_ip}"
+}
+
+The output block defines the values that are outputted when Terraform runs. In this case, the output is the public
+
 
 output "instance_arn" {
   value = aws_instance.ubuntu.arn
